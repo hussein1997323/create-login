53341185bceda815f55a1df413172d602d29b3ee
hussein1997323 <husseinothman660@gmail.com>
2024-02-13 19:42:25 -0300

    criar senha e email para login

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-alert('Bem vindo')
+function criarConta(event) {
+    event.preventDefault();
+
+    var meuemail = 'hussein@gmail.com';
+    var minhasenha = '123'; // Certifique-se de que a senha seja uma string
+
+    var email = document.querySelector('.email').value.toLowerCase();
+    var password = document.querySelector('.password').value;
+
+    if (email === meuemail && password === minhasenha) {
+       
+        window.location.href ='./html/home.html'
+        
+        return true;
+    } else {
+        alert('Por favor, preencha todos os campos corretamente.');
+        return false;
+    }
+}
