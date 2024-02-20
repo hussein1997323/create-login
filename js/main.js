083046618fca98e250a2345401f0f46faca7aaa0
hussein1997323 <husseinothman660@gmail.com>
2024-02-20 00:02:36 -0300

    super as alteração

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
+// Esta função é chamada quando o formulário de criação de conta é enviado
 function criarConta(event) {
+    // Evita o comportamento padrão do formulário de atualizar a página
     event.preventDefault();
 
+    // Obtém o valor do campo de email
     var email = document.querySelector('.email').value;
+    
+    // Obtém o valor do campo de senha
     var password = document.querySelector('.password').value;
 
+    // Verifica se o email e a senha correspondem a um conjunto específico
     if (email === 'hussein@gmail.com' && password === '123') {
-       
-        window.location.href ='./html/home.html'
-        
+        // Se as credenciais forem válidas, redireciona o usuário para a página de início
+        window.location.href ='./html/home.html';
+        // Retorna verdadeiro para permitir o envio do formulário
         return true;
     } else {
+        // Se as credenciais não forem válidas, exibe um alerta pedindo para preencher os campos corretamente
         alert('Por favor, preencha todos os campos corretamente.');
+        // Retorna falso para evitar o envio do formulário
         return false;
     }
 }
